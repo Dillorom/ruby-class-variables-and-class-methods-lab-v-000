4a69543fb2bb30ad0727cacea6235ced2f577719
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-07 17:43:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,10 +30,10 @@ class Song
   def self.genre_count
     @@genres.each do |genre|
       if !@@genre_count.key?(genre)
-        @@genre_count.value = 1
+        @@genre_count[genre] = 1
       elsif
         @@genre_count.key?(genre)
-        @@genre_count.value+=1
+        @@genre_count[genre]+=1
       end
     end
     @@genre_count
