373db03402e91c36d214f90a126a7421fcbe2e17
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-07 16:48:41 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,48 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count+=1
+  end
+
+  def self.count
+      @@count
+  end
+
+  def self.artists
+    @@artists
+    @@artists << artists if artists != self
+  end
+
+
+  def self.genres
+    @@genres
+
+  end
+
+  def self.genre_count
+      @@genres << genres if genres != self
+    end
+end
+
+class Artist
+  attr_accessor :name, 
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+end
+
+class Genre
+  attr_accessor :genre
+  def initialize(genre)
+    @genre = genre
+    @genres = []
+  end
+end
+  
